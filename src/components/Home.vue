0c3b49237129023c729d6cf42a9e12011e577376
tw <1606532751@qq.com>
2020-12-08 17:41:05 +0800

    tw用户列表开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,158 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home_container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/store.png" alt="" />
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+      <!-- 侧边栏区域 -->
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <!-- 侧边栏菜单区域 -->
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#409EFF"
+          unique-opened
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          router
+          :default-active="activePath"
+        >
+          <!-- 一级菜单 -->
+          <el-submenu
+            :index="item.id + ''"
+            v-for="item in menuList"
+            :key="item.id"
+          >
+            <!-- 一级菜单模板区域 -->
+            <template slot="title">
+              <i :class="iconsObj[item.id]"></i>
+              <span>{{ item.authName }}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item
+              :index="'/' + subItem.path"
+              v-for="subItem in item.children"
+              :key="subItem.id"
+              @click="saveNavState('/' + subItem.path)"
+            >
+              <template slot="title">
+                <i class="el-icon-menu"></i>
+                <span>{{ subItem.authName }}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧内容区域 -->
+      <el-main>
+        <!-- 路由占位符 -->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      // 左侧菜单数据
+      menuList: [],
+      // 图标
+      iconsObj: {
+        125: 'iconfont icon-group',
+        103: 'iconfont icon-quanxian',
+        101: 'iconfont icon-shangpin',
+        102: 'iconfont icon-dingdan',
+        145: 'iconfont icon-shujutongji'
+      },
+      // 默认是否折叠
+      isCollapse: false,
+      // 保存激活链接地址
+      activePath: ''
+    }
+  },
+  created () {
+    this.getMenuList()
+    this.activePath = window.sessionStorage.getItem('activePath')
+  },
   methods: {
+    //   点击退出
     logout () {
       window.sessionStorage.clear()
       this.$router.push('/login')
       this.$message.success('退出成功！')
+    },
+    // 获取所有的菜单
+    async getMenuList () {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      console.log(res)
+      this.menuList = res.data
+    },
+    // 点击按钮，展开与折叠函数
+    toggleCollapse () {
+      this.isCollapse = !this.isCollapse
+    },
+    // 保存链接的激活状态
+    saveNavState (activePath) {
+      window.sessionStorage.setItem('activePath', activePath)
+      this.activePath = activePath
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
+.home_container {
+  height: 100%;
+  .el-header {
+    display: flex;
+    justify-content: space-between;
+    // padding-left: 10px;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+    background-color: #373d41;
+    div {
+      display: flex;
+      align-items: center;
+      img {
+        width: 50px;
+        height: 50px;
+      }
+      span {
+        margin-left: 15px;
+      }
+    }
+  }
+  .el-aside {
+    background-color: #333744;
+    .toggle-button {
+      background-color: #4a5064;
+      font-size: 10px;
+      line-height: 24px;
+      color: #fff;
+      text-align: center;
+      letter-spacing: 0.4em;
+      cursor: pointer;
+    }
+    .el-menu {
+      border-right: none;
+      .iconfont {
+        margin-right: 10px;
+      }
+    }
+  }
+  .el-main {
+    background-color: #eaedf1;
+  }
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,11 +1,20 @@
 import Vue from 'vue'
 // 按需导入的标签
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItem } from 'element-ui'
 
 Vue.use(Button)
 // 组件注册为全局可用
+// 登陆页面的
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+// 主页的
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
 // $自定义名字 = 正确组件名
 Vue.prototype.$message = Message
